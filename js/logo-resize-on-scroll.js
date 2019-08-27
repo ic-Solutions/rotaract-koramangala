b019b09966e9b431358f79d5cbb4424c5a18cd2c
sharu725 <sharu725@gmail.com>
2019-08-27 08:29:59 +0530

    initialize

---
+++ js/logo-resize-on-scroll.js
@@ -0,0 +1,12 @@
+window.onscroll = function() {
+  growShrinkLogo()
+};
+
+function growShrinkLogo() {
+  var Logo = document.getElementById("Logo")
+  if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
+    Logo.style.height = '60px';
+  } else {
+    Logo.style.height = '150px';
+  }
+}
