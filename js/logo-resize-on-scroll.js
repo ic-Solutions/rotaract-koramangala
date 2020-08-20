4e156d3ca0ea7b52aab9a3fb29b7cf70bc440d57
icsdevs <usprajwalkumar@gmail.com>
2020-08-20 13:48:49 +0530

    1. Added Area of focus 2. Added back to top button 3. Added 2 videos to club services in video page 4. Added BOD 2020 2021

--- js/logo-resize-on-scroll.js
+++ js/logo-resize-on-scroll.js
@@ -1,6 +1,9 @@
+var mybutton = document.getElementById("toTop");
+
 window.onscroll = function() {
   growShrinkLogo();
-    hideMargin()
+    hideMargin();
+    scrollFunction()
 };
 
 function growShrinkLogo() {
@@ -21,4 +24,17 @@ function hideMargin (){
     } else {
         menu_id.style.top = '35px'
     }
+}
+
+function scrollFunction() {
+  if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
